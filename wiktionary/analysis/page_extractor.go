290079db8a49f0e40f44961bbdca619f72100388
pollen <seth.pollen@gmail.com>
2016-12-24 22:23:37 -0600

    Move wiktionary out of incubator

---
+++ wiktionary/analysis/page_extractor.go
@@ -0,0 +1,38 @@
+// Utilities for working with the Wiktionary XML dump.
+
+package analysis
+
+import (
+	"encoding/xml"
+	"io"
+)
+
+// Result type of the XML parser; represents one MediaWiki page.
+type Page struct {
+	Title string `xml:"title"`
+	Text  string `xml:"revision>text"`
+}
+
+type ProcessPage func(page *Page)
+
+// Reads the full XML dump from 'file' and invokes 'process' on each page found
+// in the dump.
+func ReadDump(file io.Reader, process ProcessPage) {
+	decoder := xml.NewDecoder(file)
+
+	for {
+		rawToken, _ := decoder.Token()
+		if rawToken == nil {
+			break
+		}
+
+		switch token := rawToken.(type) {
+		case xml.StartElement:
+			if token.Name.Local == "page" {
+				var page Page
+				decoder.DecodeElement(&page, &token)
+				process(&page)
+			}
+		}
+	}
+}
---
+++ wiktionary/inflection_xml.go
@@ -0,0 +1,19 @@
+// Defines an encoding/xml schema for storing inflection data as XML.
+
+package wiktionary
+
+import (
+	"encoding/xml"
+)
+
+type Inflections struct {
+	XMLName     xml.Name     `xml:"inflections"`
+	Inflections []Inflection `xml:"inflection"`
+}
+
+type Inflection struct {
+	XMLName        xml.Name `xml:"inflection"`
+	BaseWord       string   `xml:"base"`
+	Pos            string   `xml:"pos"`
+	InflectedForms []string `xml:"inflected"`
+}
